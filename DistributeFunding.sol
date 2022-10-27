9c116a0e87a125de03ce49d15ff3be4f314813a0
AndreiG <48528839+AndreiGhiran@users.noreply.github.com>
2022-10-27 12:55:14 +0300

    implementare DistributeFunding
    o prima implementare a contractului.

--- DistributeFunding.sol
+++ DistributeFunding.sol
@@ -4,7 +4,36 @@ pragma solidity >=0.7.0 <0.9.0;
 
 contract DistributeFunding {
 
-    function distributeFunding() public {
-        
+	bool funded = false;
+    uint total_stake = 0;
+    mapping(address => uint) benefactor_stakes;
+
+    function addBenefector(address payable user, uint stake) external {
+        require(!funded,"Funded");
+        benefactor_stakes[user] = stake;
     }
+
+    function getBenefactorStake(address user) view external returns(uint){
+        return benefactor_stakes[user];
+    }
+
+    function receiveFunds() payable external {
+        require(msg.value > 0);
+        funded = true;
+    }
+
+    function getFunds() external {
+        require(benefactor_stakes[msg.sender] != 0 && funded, "No funds available");
+        uint amount = (benefactor_stakes[msg.sender] * address(this).balance) / 100;
+        payable(msg.sender).transfer(amount);
+        benefactor_stakes[msg.sender] = 0;
+    }
+
+    receive() payable external{}
+
+    fallback () external {}
+	
+	//function distributeFunding() public {
+    //    
+    //}
 }
